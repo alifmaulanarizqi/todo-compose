5b16a7b401d31af62d073d7bcb64f974f5f37a26
alif <alifmaulana312@gmail.com>
2026-01-02 10:41:20 +0700

    feat: add search app bar

--- app/src/main/java/com/example/todocompose/ui/screen/list/ListAppBar.kt
+++ app/src/main/java/com/example/todocompose/ui/screen/list/ListAppBar.kt
@@ -1,7 +1,13 @@
 package com.example.todocompose.ui.screen.list
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -9,7 +15,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -18,22 +27,36 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.ContentAlpha
 import com.example.todocompose.ui.theme.topAppBarBackgroundColor
 import com.example.todocompose.ui.theme.topAppBarContentColor
 import com.example.todocompose.R
 import com.example.todocompose.component.PriorityItem
 import com.example.todocompose.data.model.Priority
 import com.example.todocompose.ui.theme.LARGE_PADDING
+import com.example.todocompose.ui.theme.TONAL_APP_BAR_ELEVATION
+import com.example.todocompose.ui.theme.TOP_APP_BAR_HEIGHT
 
 @Composable
 fun ListAppBar() {
-    DefaultListAppBar(
-        onSearchClicked = { },
-        onSortClicked = { },
-        onDeleteClicked = {},
+//    DefaultListAppBar(
+//        onSearchClicked = { },
+//        onSortClicked = { },
+//        onDeleteClicked = {},
+//    )
+
+    SearchAppBar(
+        text = "",
+        onTextChange = {},
+        onCloseClicked = {},
+        onSearchClicked = {}
     )
 }
 
@@ -189,11 +212,94 @@ fun DeleteAllAction(
 }
 
 @Composable
+fun SearchAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding(),
+        tonalElevation = TONAL_APP_BAR_ELEVATION,
+        color = MaterialTheme.colorScheme.topAppBarBackgroundColor
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = text,
+            colors = TextFieldDefaults.colors(
+                cursorColor = MaterialTheme.colorScheme.topAppBarContentColor,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent
+            ),
+            onValueChange = { onTextChange(it) },
+            placeholder = {
+                Text(
+                    text = "Search",
+                    color = Color.White,
+                    modifier = Modifier.alpha(ContentAlpha.medium)
+                )
+            },
+            textStyle = TextStyle(
+                color = MaterialTheme.colorScheme.topAppBarContentColor,
+                fontSize = MaterialTheme.typography.titleSmall.fontSize
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier.alpha(ContentAlpha.disabled)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon",
+                        tint = MaterialTheme.colorScheme.topAppBarContentColor
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = { onCloseClicked() }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close Icon",
+                        tint = MaterialTheme.colorScheme.topAppBarContentColor
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = { onSearchClicked(text) }
+            )
+        )
+    }
+}
+
+@Composable
 @Preview
 private fun DefaultListAppBarPreview() {
     DefaultListAppBar(
         onSearchClicked = {},
         onSortClicked = {},
         onDeleteClicked = {}
+    )
+}
+
+@Composable
+@Preview
+fun SearchAppBarPreview() {
+    SearchAppBar(
+        text = "",
+        onTextChange = {},
+        onCloseClicked = {},
+        onSearchClicked = {}
     )
 }
--- app/src/main/java/com/example/todocompose/ui/screen/list/ListScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screen/list/ListScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todocompose.R
