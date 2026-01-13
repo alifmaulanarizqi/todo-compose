376b098d10371fdb4cc88b3ce4baaa975f2c7357
alif <alifmaulana312@gmail.com>
2026-01-13 17:13:16 +0700

    feat: add task screen content

---
+++ app/src/main/java/com/example/todocompose/ui/screen/task/TaskContent.kt
@@ -0,0 +1,77 @@
+package com.example.todocompose.ui.screen.task
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todocompose.data.model.Priority
+import com.example.todocompose.ui.theme.LARGE_PADDING
+import com.example.todocompose.R
+import com.example.todocompose.component.PriorityDropDown
+import com.example.todocompose.ui.theme.MEDIUM_PADDING
+
+@Composable
+fun TaskContent(
+    modifier: Modifier = Modifier,
+    title: String,
+    onTitleChange: (String) -> Unit,
+    description: String,
+    onDescriptionChange: (String) -> Unit,
+    priority: Priority,
+    onPrioritySelected: (Priority) -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(all = LARGE_PADDING)
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = title,
+            onValueChange = { onTitleChange(it) },
+            label = { Text(text = stringResource(R.string.title_label)) },
+            textStyle = MaterialTheme.typography.bodyMedium,
+            singleLine = true
+        )
+        Divider(
+            modifier = Modifier.height(MEDIUM_PADDING),
+            color = MaterialTheme.colorScheme.background
+        )
+        PriorityDropDown(
+            priority = priority,
+            onPrioritySelected = onPrioritySelected
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxSize(),
+            value = description,
+            onValueChange = { onDescriptionChange(it) },
+            label = { Text(text = stringResource(R.string.description_label)) },
+            textStyle = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Composable
+@Preview
+fun TaskContentPreview() {
+    TaskContent(
+        title = "",
+        onTitleChange = {},
+        description = "",
+        onDescriptionChange = {},
+        priority = Priority.LOW,
+        onPrioritySelected = {}
+    )
+}
--- app/src/main/java/com/example/todocompose/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screen/task/TaskScreen.kt
@@ -1,8 +1,11 @@
 package com.example.todocompose.ui.screen.task
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.todocompose.data.model.Priority
 import com.example.todocompose.data.model.ToDoTaskEntity
 import com.example.todocompose.util.Action
 
@@ -19,6 +22,16 @@ fun TaskScreen(
                 selectedTask = selectedTask
             )
         },
-        content = {}
+        content = { paddingValues ->
+            TaskContent(
+                modifier = Modifier.padding(paddingValues),
+                title = "",
+                onTitleChange = {},
+                description = "",
+                onDescriptionChange = {},
+                priority = Priority.LOW,
+                onPrioritySelected = {}
+            )
+        }
     )
 }
